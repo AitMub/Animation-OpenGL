3457530a656b01cd245c9bc505b4b9bb6df83eb7
AitMub <89588827+AitMub@users.noreply.github.com>
2022-10-22 18:28:24 +0800

    Add math related template function

---
+++ Animation/utility/anim_math.cpp
@@ -0,0 +1,43 @@
+#include"anim_math.h"
+
+template<>
+mat4 Convert(const aiMatrix4x4& ai_mat4) {
+    mat4 mat = glm::mat4();
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            mat[j][i] = ai_mat4[i][j];
+        }
+    }
+    return mat;
+}
+
+template<>
+quat Convert(const aiQuaternion& ai_quat) {
+    return glm::quat(ai_quat.w, ai_quat.x, ai_quat.y, ai_quat.z);
+}
+
+template<>
+mat4 Convert(const aiQuaternion& ai_quat) {
+    return glm::mat4_cast(Convert<quat>(ai_quat));
+}
+
+template<>
+vec3 Convert(const aiVector3D& ai_vec) {
+    return vec3(ai_vec.x, ai_vec.y, ai_vec.z);
+}
+
+template<>
+float Convert(const aiVector3D& ai_vec) {
+    return ai_vec.x;
+}
+
+template<typename T>
+T Interpolate(const T& value1, const T& value2, float factor) {
+    return (value2 - value1) * factor + value1;
+}
+
+quat Interpolate(const quat& value1, const quat& value2, float factor) {
+    return glm::slerp(value1, value2, factor);
+}
---
+++ Animation/utility/anim_math.h
@@ -0,0 +1,41 @@
+#ifndef  ANIM_MATH_H
+#define ANIM_MATH_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/detail/type_quat.hpp>
+#include <glm/ext/quaternion_common.hpp>
+#include <assimp/Importer.hpp>
+using glm::mat4;
+using glm::quat;
+using glm::vec3;
+
+// Converter
+template<typename T1, typename T2>
+T1 Convert(const T2& src);
+
+template<>
+mat4 Convert(const aiMatrix4x4& ai_mat4);
+
+template<>
+quat Convert(const aiQuaternion& ai_quat);
+
+template<>
+mat4 Convert(const aiQuaternion& ai_quat);
+
+template<>
+vec3 Convert(const aiVector3D& ai_vec);
+
+template<>
+float Convert(const aiVector3D& ai_vec);
+
+
+// Interpolate
+template<typename T>
+T Interpolate(const T& value1, const T& value2, float factor);
+
+template<>
+quat Interpolate<quat>(const quat& value1, const quat& value2, float factor);
+
+#endif
